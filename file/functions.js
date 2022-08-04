f9af3038711a1d4087bc3e72806aa834c2585b22
Yozee <mayuzhe1222@qq.com>
2022-08-04 21:00:51 +0800

    修改

--- file/functions.js
+++ file/functions.js
@@ -29,15 +29,15 @@ $(window).resize(function() {
 				var current = str.substr(progress, 1);
 				if (current == '<') {
 					progress = str.indexOf('>', progress) + 1;
+					scrolltest()
 				} else {
 					progress++;
 				}
 				$ele.html(str.substring(0, progress) + (progress & 1 ? '_' : ''));
 				if (progress >= str.length) {
 					clearInterval(timer);
-
 				}
-				scrolltest()
+
 			}, 75);
 		});
 		return this;
